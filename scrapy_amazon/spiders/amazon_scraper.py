6cab2a2bb6f95a831ecde68b903bf497a8655c23
Gaurav Agrawal <GauravAgrawal1108>
2024-04-16 08:07:24 +0530

    amazon product scraper

--- scrapy_amazon/spiders/amazon_scraper.py
+++ scrapy_amazon/spiders/amazon_scraper.py
@@ -15,7 +15,7 @@ class AmazonScraperSpider(scrapy.Spider):
                         "CONCURRENT_REQUESTS" : 2,
                         "DOWNLOAD_DELAY" : .3,
                             
-                    }
+                    } 
     
     header = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
